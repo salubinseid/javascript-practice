b4c4de52f433b0a7a7793c531e753eddb8eb7c93
Sal_Mac <mio_macbook@Sal-Macbook.local>
2025-10-05 14:28:15 +0300

    Initial repo structure for JavaScript practice

---
+++ control-flow/if-else.js
@@ -0,0 +1,8 @@
+
+let num1 = 10
+
+if (num1 > 20){
+    console.log("The number is greater than 20")
+}else{
+    console.log("The number is less than 20")
+}
---
+++ control-flow/loops.js
@@ -0,0 +1,11 @@
+
+for (i=1;i<10; i++){
+    for (j=0;j<10;j++){
+        console.log(i*j)
+    }
+}
+
+isTrue = false;
+while (isTrue == true){
+    console.log("This is correct")
+}
